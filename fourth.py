6750a480bd94f2df165758fffaf5cdc7fd32a51d
Sererim <danielleymanw@gmail.com>
2023-07-16 18:08:55 +0300

    Final commit.

--- fourth.py
+++ fourth.py
@@ -2,3 +2,19 @@
 # Не учитывать знаки препинания и регистр символов.
 # За основу возьмите любую статью из википедии или из документации к языку.
 # Статья находится в документе wiki.txt
+
+f = open("wiki.txt", "r")
+
+lst = list(f)
+text_raw = []
+
+for i in lst:
+    str(i).strip(",.")
+    text_raw += (str(i).split())
+
+for i in range(len(lst)):
+    d = {x: text_raw.count(x) for x in text_raw}
+
+for k,v in d.items():
+    if v > 10:
+        print(f"{k}: {v}")
