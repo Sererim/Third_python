6c2de43c9e4b23d8c4b1acf045ebfc2a2355747a
Sererim <danielleymanw@gmail.com>
2023-07-16 17:29:15 +0300

    Final commit.

--- second.py
+++ second.py
@@ -2,9 +2,10 @@
 # Удалите из него все элементы, которые встречаются дважды.
 
 def main():
-    l = [x for x in range(101)]
-    l += l[::-1]
-    print(l)
+    l = [x + 1 for x in range(101)]
+    lst = l[::2] + l[::2] + l[::-3]
+    no_duplicates = set(lst)
+    print(f"Origianl list:\n{lst}\nNo duplicates list:\n{list(no_duplicates)}")
     return 0
 
 
